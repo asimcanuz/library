c6ae8e93ce41ec7f09ab08e89420feb578241093
asimcanuz <asimcanuz@gmail.com>
2024-05-03 00:16:09 +0300

    create AuthorService

---
+++ src/main/java/com/asodev/library/service/AuthorServiceImpl.java
@@ -0,0 +1,72 @@
+package com.asodev.library.service;
+
+import com.asodev.library.dto.AuthorDTO;
+import com.asodev.library.model.Author;
+import com.asodev.library.repository.AuthorRepository;
+import com.asodev.library.service.interfaces.AuthorService;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+    private final ModelMapper modelMapper;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository, ModelMapper modelMapper) {
+        this.authorRepository = authorRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    private AuthorDTO convertToDto(Author author){
+        return modelMapper.map(author,AuthorDTO.class);
+    }
+    private Author convertToEntity(AuthorDTO authorDTO){
+        return modelMapper.map(authorDTO,Author.class);
+    }
+
+    @Override
+    public AuthorDTO createAuthor(AuthorDTO authorDTO) {
+        Author author = convertToEntity(authorDTO);
+        author = authorRepository.save(author);
+        return convertToDto(author);
+    }
+
+    @Override
+    public AuthorDTO getAuthorById(Long id) {
+        Author author = getAuthor(id);
+        return convertToDto(author);
+    }
+
+    @Override
+    public List<AuthorDTO> getAllAuthors() {
+        List<Author> authors = authorRepository.findAll();
+        return authors.stream().map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AuthorDTO updateAuthor(Long id, AuthorDTO authorDTO) {
+        Author author = getAuthor(id);
+        author.setFirstName(authorDTO.getFirstName());
+        author.setLastName(authorDTO.getLastName());
+        author = authorRepository.save(author);
+
+        return convertToDto(author);
+    }
+
+    @Override
+    public void deleteAuthor(Long id) {
+
+    }
+
+
+    private Author getAuthor(Long id){
+        Author author = authorRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException(id+" id'li ürün bulunamadı!"));
+        return author;
+    }
+
+
+}
---
+++ src/main/java/com/asodev/library/service/interfaces/AuthorService.java
@@ -0,0 +1,17 @@
+package com.asodev.library.service.interfaces;
+
+import com.asodev.library.dto.AuthorDTO;
+
+import java.util.List;
+
+public interface AuthorService {
+    AuthorDTO createAuthor(AuthorDTO authorDTO);
+
+    AuthorDTO getAuthorById(Long id);
+
+    List<AuthorDTO> getAllAuthors();
+
+    AuthorDTO updateAuthor(Long id, AuthorDTO authorDTO);
+
+    void deleteAuthor(Long id);
+}
