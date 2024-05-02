87b30e8d2ad68f5dc9fd257399d908b51d83ff0a
asimcanuz <asimcanuz@gmail.com>
2024-05-03 00:03:43 +0300

    Book repository query eklendi

--- src/main/java/com/asodev/library/repository/BookRepository.java
+++ src/main/java/com/asodev/library/repository/BookRepository.java
@@ -3,5 +3,13 @@ package com.asodev.library.repository;
 import com.asodev.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book,Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthorFirstNameAndAuthorLastNameIgnoreCase(String authorFirstName, String authorLastName);
+
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorFirstNameContainingIgnoreCaseOrAuthorLastNameContainingIgnoreCase(String title, String authorFirstName, String authorLastName);
+
 }
