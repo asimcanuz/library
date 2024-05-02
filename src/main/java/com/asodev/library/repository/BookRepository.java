47b8577dfb2fe321c93c7ee397c2b9d88540ae58
asimcanuz <asimcanuz@gmail.com>
2024-05-03 01:02:42 +0300

    FIX add repository annotation

--- src/main/java/com/asodev/library/repository/BookRepository.java
+++ src/main/java/com/asodev/library/repository/BookRepository.java
@@ -2,9 +2,11 @@ package com.asodev.library.repository;
 
 import com.asodev.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
     List<Book> findByTitleContainingIgnoreCase(String title);
 
--- src/main/java/com/asodev/library/repository/LoanRepository.java
+++ src/main/java/com/asodev/library/repository/LoanRepository.java
@@ -2,6 +2,8 @@ package com.asodev.library.repository;
 
 import com.asodev.library.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoanRepository extends JpaRepository<Loan,Long> {
 }
