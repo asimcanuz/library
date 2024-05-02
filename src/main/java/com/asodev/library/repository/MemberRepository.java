740fd6f14538308074385eea5a4b8ee81e8e5d55
asimcanuz <asimcanuz@gmail.com>
2024-05-03 01:03:39 +0300

    FIX member repository

--- src/main/java/com/asodev/library/repository/MemberRepository.java
+++ src/main/java/com/asodev/library/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.asodev.library.repository;
 
+import com.asodev.library.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<MemberRepository,Long> {
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
