a49b7102f03971329f0588e26160f29cbc30b45b
asimcanuz <asimcanuz@gmail.com>
2024-05-03 01:22:40 +0300

    add swagger and description

--- src/main/java/com/asodev/library/controller/AuthorController.java
+++ src/main/java/com/asodev/library/controller/AuthorController.java
@@ -2,6 +2,7 @@ package com.asodev.library.controller;
 
 import com.asodev.library.dto.AuthorDTO;
 import com.asodev.library.service.interfaces.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,30 +20,35 @@ public class AuthorController  {
         this.authorService = authorService;
     }
 
+    @Operation(summary = "Yeni yazar ekle")
     @PostMapping
     public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO authorDTO) {
         AuthorDTO createdAuthor = authorService.createAuthor(authorDTO);
         return new ResponseEntity<>(createdAuthor, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Id ile yazar getir")
     @GetMapping("/{id}")
     public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
         AuthorDTO author = authorService.getAuthorById(id);
         return new ResponseEntity<>(author, HttpStatus.OK);
     }
 
+    @Operation(summary = "Bütün yazarları getir")
     @GetMapping
     public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
         List<AuthorDTO> authors = authorService.getAllAuthors();
         return new ResponseEntity<>(authors, HttpStatus.OK);
     }
 
+    @Operation(summary = "Yazar güncelle")
     @PutMapping("/{id}")
     public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable Long id, @RequestBody AuthorDTO authorDTO) {
         AuthorDTO updatedAuthor = authorService.updateAuthor(id, authorDTO);
         return new ResponseEntity<>(updatedAuthor, HttpStatus.OK);
     }
 
+    @Operation(summary = "Author sil")
     @PatchMapping("/{id}")
     public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
         authorService.deleteAuthor(id);
--- src/main/java/com/asodev/library/controller/BookController.java
+++ src/main/java/com/asodev/library/controller/BookController.java
@@ -2,6 +2,7 @@ package com.asodev.library.controller;
 
 import com.asodev.library.dto.BookDTO;
 import com.asodev.library.service.interfaces.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,48 +18,55 @@ public class BookController{
         this.bookService = bookService;
     }
 
+    @Operation(summary = "Id ile kitap getir")
     @GetMapping("/{id}")
     public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
         BookDTO book = bookService.getBookById(id);
         return new ResponseEntity<>(book, HttpStatus.OK);
     }
-
+    @Operation(summary = "Kitapları getir")
     @GetMapping
     public ResponseEntity<List<BookDTO>> getAllBooks() {
         List<BookDTO> books = bookService.getAllBooks();
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
+    @Operation(summary = "Kitapları ismine göre getir")
     @GetMapping("/name/{name}")
     public ResponseEntity<List<BookDTO>> getBooksByName(@PathVariable String name) {
         List<BookDTO> books = bookService.getBooksByName(name);
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
+    @Operation(summary = "Kitapları yazara göre getir")
     @GetMapping("/author")
     public ResponseEntity<List<BookDTO>> getBooksByAuthorName(@RequestParam String authorFirstName, @RequestParam String authorLastName) {
         List<BookDTO> books = bookService.getBooksByAuthorName(authorFirstName, authorLastName);
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
+    @Operation(summary = "Kitapları yazar ve isme göre getir")
     @GetMapping("/search")
     public ResponseEntity<List<BookDTO>> searchBooks(@RequestParam String searchTerm) {
         List<BookDTO> books = bookService.searchBooks(searchTerm);
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
+    @Operation(summary = "Kitap oluştur")
     @PostMapping
     public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO bookDTO) {
         BookDTO createdBook = bookService.createBook(bookDTO);
         return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Kitap güncelle")
     @PutMapping("/{id}")
     public ResponseEntity<BookDTO> updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
         BookDTO updatedBook = bookService.updateBook(id, bookDTO);
         return new ResponseEntity<>(updatedBook, HttpStatus.OK);
     }
 
+    @Operation(summary = "Kitap sil")
     @PatchMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
