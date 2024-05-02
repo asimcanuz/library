6c9714c71f321e3ae3dd999247b73d3b2a330f81
asimcanuz <asimcanuz@gmail.com>
2024-05-03 00:49:14 +0300

    add author controller

---
+++ src/main/java/com/asodev/library/controller/AuthorControllerImpl.java
@@ -0,0 +1,52 @@
+package com.asodev.library.controller;
+
+import com.asodev.library.controller.interfaces.AuthorController;
+import com.asodev.library.dto.AuthorDTO;
+import com.asodev.library.service.interfaces.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/authors")
+public class AuthorControllerImpl implements AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorControllerImpl(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO authorDTO) {
+        AuthorDTO createdAuthor = authorService.createAuthor(authorDTO);
+        return new ResponseEntity<>(createdAuthor, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
+        AuthorDTO author = authorService.getAuthorById(id);
+        return new ResponseEntity<>(author, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+        List<AuthorDTO> authors = authorService.getAllAuthors();
+        return new ResponseEntity<>(authors, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable Long id, @RequestBody AuthorDTO authorDTO) {
+        AuthorDTO updatedAuthor = authorService.updateAuthor(id, authorDTO);
+        return new ResponseEntity<>(updatedAuthor, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
