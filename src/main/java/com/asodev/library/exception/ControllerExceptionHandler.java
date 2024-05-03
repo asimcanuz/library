edcfef87d6d53ddda32587eec7b4183c28e96cf4
asimcanuz <asimcanuz@gmail.com>
2024-05-04 00:05:37 +0300

    add controller advice

---
+++ src/main/java/com/asodev/library/exception/ControllerExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.asodev.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest req){
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return new ResponseEntity<ErrorMessage>(message,HttpStatus.NOT_FOUND);
+    }    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest req){
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return new ResponseEntity<ErrorMessage>(message,HttpStatus.NOT_FOUND);
+    }
+
+
+
+}
