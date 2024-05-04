a9021c9178c9aa841469cd6282841f6579220bab
asimcanuz <asimcanuz@gmail.com>
2024-05-04 23:16:52 +0300

    add Role model

---
+++ src/main/java/com/asodev/library/model/Role.java
@@ -0,0 +1,22 @@
+package com.asodev.library.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_USER("USER"),
+    ROLE_ADMIN("ADMIN"),
+    ROLE_MOD("MOD");
+    private final String value;
+    Role(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
