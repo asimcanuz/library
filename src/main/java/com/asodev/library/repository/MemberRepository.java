07382dee95ce741c6c0a94aa3bdb414bf1b01b02
asimcanuz <asimcanuz@gmail.com>
2024-05-04 23:15:47 +0300

    rename to Member from User

--- src/main/java/com/asodev/library/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asodev.library.repository;
-
-import com.asodev.library.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
